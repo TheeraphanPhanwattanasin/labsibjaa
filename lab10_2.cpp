df613a7a55b1aaad5b4a092be783f0e378f728a1
boss <theeraphan_p@cmu.ac.th>
2022-12-27 16:27:47 +0700

    10.2 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+#include <iostream>
+
+void printO(int M, int N){
+
+if(M <= 0 || N<=0){
+
+cout << "Invalid input";
+
+}
+
+else{
+for(int i=0;i<M;i++){
+
+    for(int j=0; j<N; j++){
+    cout << "O";
+    }
+
+cout << "\n";
+}
+}
+
+}
 
 int main(){
 	
